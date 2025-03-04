7681e934872d1a33549edc9f5b5cc788641bbb59
playgame <hongjian1122@sina.com>
2025-03-04 21:43:48 +0800

    添加播放网络音乐

--- core/handle/musicHandler.py
+++ core/handle/musicHandler.py
@@ -1,3 +1,6 @@
+from urllib.parse import urlencode
+
+import requests
 from config.logger import setup_logging
 import os
 import random
@@ -8,6 +11,9 @@ from core.handle.sendAudioHandle import send_stt_message
 
 TAG = __name__
 logger = setup_logging()
+
+MusicApiUrl = "https://y.0msl.com/"
+lrc_pattern = re.compile(r"\[(\d{2}):(\d{2})\.(\d{2})\](.*)")
 
 
 def _extract_song_name(text):
@@ -44,9 +50,13 @@ class MusicHandler:
             self.music_dir = os.path.abspath(
                 self.music_config.get("music_dir", "./music")  # 默认路径修改
             )
+            self.music_cache_dir=os.path.abspath(
+                self.music_config.get("music_cache_dir","./music/cache") #缓存路径
+            )
             self.music_related_keywords = self.music_config.get("music_commands", [])
         else:
             self.music_dir = os.path.abspath("./music")
+            self.music_cache_dir=os.path.abspath("./music/cache")
             self.music_related_keywords = ["来一首歌", "唱一首歌", "播放音乐", "来点音乐", "背景音乐", "放首歌",
                                            "播放歌曲", "来点背景音乐", "我想听歌", "我要听歌", "放点音乐"]
 
@@ -67,10 +77,13 @@ class MusicHandler:
                     logger.bind(tag=TAG).info(f"找到最匹配的歌曲: {best_match}")
                     await self.play_local_music(conn, specific_file=best_match)
                     return True
+                else:
+                    await self.play_net_music(conn,potential_song)
+                    return True
 
         # 检查是否是通用播放音乐命令
         if any(cmd in clean_text for cmd in self.music_related_keywords):
-            await self.play_local_music(conn)
+            await self.play_net_music(conn)
             return True
 
         return False
@@ -108,3 +121,65 @@ class MusicHandler:
         except Exception as e:
             logger.bind(tag=TAG).error(f"播放音乐失败: {str(e)}")
             logger.bind(tag=TAG).error(f"详细错误: {traceback.format_exc()}")
+
+    async def play_net_music(self,conn,song_name=None):
+        if song_name is None:
+            music_list=["卜卦","土坡上的狗尾草","没关系","一点","越来越不懂","特别的人","唯一","舍得","平凡日子里的挣扎"]
+            select_music=random.choice(music_list)
+        else:
+            select_music=song_name
+        try:
+            payload = {
+                "input": select_music,
+                "filter": "name",
+                "type": "netease",
+                "page": "1",
+            }
+            encode_data = urlencode(payload)
+            headers = {
+                "Content-Type": "application/x-www-form-urlencoded",
+                "x-requested-with": "XMLHttpRequest",
+            }
+            response = requests.post(MusicApiUrl, data=encode_data, headers=headers)
+            response.raise_for_status()
+            # logger.bind(tag=TAG).info(f"{response.json()}")
+            response_data = response.json()
+            if response_data["code"] != 200:
+                logger.bind(tag=TAG).error(
+                    f"获取歌曲链接失败！{response_data['error']}"
+                )
+            for song in response_data["data"]:
+                song_type = song["type"]
+                song_link = song["link"]
+                song_id = song["songid"]
+                song_title = song["title"]
+                song_lrc = song["lrc"]
+                song_url = song["url"]
+                song_pic = song["pic"]
+                
+                logger.bind(tag=TAG).info(f"title:{song_title}, url:{song_url}")
+                # 获取跳转前链接
+                jump_response = requests.get(song_url, stream=True)
+                jump_response.raise_for_status()
+                if "audio/mpeg" in jump_response.headers["Content-Type"]:
+                    # 获取到有效链接
+                    # 保存文件
+                    song_file_name = "song.mp3"
+                    save_path = os.path.join(self.music_cache_dir, song_file_name)
+                    with open(save_path, "wb") as file:
+                        for chunk in jump_response.iter_content(chunk_size=1024):
+                            file.write(chunk)
+                    print(f"文件已下载到:{save_path}")
+                    text = f"正在播放{song_title}"
+                    await send_stt_message(conn, text)
+                    conn.tts_first_text = song_title
+                    conn.tts_last_text = song_title
+                    conn.llm_finish_task = True
+                    if save_path.endswith(".p3"):
+                        opus_packets, duration = p3.decode_opus_from_file(save_path)
+                    else:
+                        opus_packets, duration = conn.tts.wav_to_opus_data(save_path)
+                    conn.audio_play_queue.put((opus_packets, song_title))
+                    break
+        except Exception as e:
+            logger.bind(tag=TAG).error(f"获取网络音乐列表失败:{traceback.format_exc()}")
